050b15b46145db26b96484f2f622d799a93fe46d
Suzanne Jucaitis <suzanne.jucaitis@gmail.com>
2021-02-10 20:43:39 -0500

    server update

---
+++ server.js
@@ -0,0 +1,18 @@
+c
+const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
+
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+app.use('/', routes);
+app.use('/', require('./routes/index'))
+
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
